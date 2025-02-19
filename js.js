99a74e1ec6f6bd80e9aa37f07f7184acd3ce0b86
Mohammadrasool Vahid <121235436+mutopia7@users.noreply.github.com>
2025-02-19 16:25:06 +0330

    feat: Add hover effect to grid cells
    - Implemented a `mouseenter` event listener for each cell.
    - Changed the background color to pink when hovered.
    - Improved user interaction and visual feedback.
    
    This update enhances the grid by making it more interactive.

--- js.js
+++ js.js
@@ -18,15 +18,27 @@ const gridContainer = document.querySelector("#gridContainer");
 const gridSize = 16; // تعداد سطرها و ستون‌ها
 
 for (let i = 0; i < gridSize; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
+  let row = document.createElement("div");
+  row.classList.add("row");
 
-    for (let j = 0; j < gridSize; j++) {
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
-        row.appendChild(cell);
-    }
+  for (let j = 0; j < gridSize; j++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    row.appendChild(cell);
+  }
 
-    gridContainer.appendChild(row);
-}
+  gridContainer.appendChild(row);
+};
+
+
+let cells = document.querySelectorAll(".cell");
+cells.forEach((cell) =>{
+  cell.addEventListener("mouseenter", function(e){
+    e.target.style.backgroundColor = "pink";
+  })
+  });
+
+
+
+
 
